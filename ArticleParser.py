aacc857aad106a699dac658181b4b145e81e0011
minsookim <minsookim0615@gmail.com>
2019-02-14 09:14:32 +0900

    function to mysql update query added

--- ArticleParser.py
+++ ArticleParser.py
@@ -4,29 +4,11 @@ from bs4 import BeautifulSoup
 import re
 import newspaper
 import json
-def something():
-    with open('all.json') as json_data_file:
-        data0 = json.load(json_data_file)
-    for i in data0:
-        url = data0[i][0]['links'][0]['href']
-        session = requests.Session()
-        session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-        # url = "http://news.khan.co.kr/kh_news/khan_art_view.html?artid=201901300748001&code=970100&utm_campaign=rss_btn_click&utm_source=khan_rss&utm_medium=rss&utm_content=total_news"
-        response = session.get(url)
-        html = response.content
-        # # paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-        #
-        article = newspaper.Article('')
-        article.set_html(html)
-        article.parse()
-        print("******************")
-        print(i)
-        print(article.title)
-        print(article.authors)
-        print(article.text)
+import pymysql
 
 class ArticleParser:
     url_list = []
+    article_list = []
     def __init__(self):
         #객체화하는 걸 완전 잊었다.... 그렇게 하는 거였지..
         with open('config.json') as json_data_file:
@@ -37,11 +19,12 @@ class ArticleParser:
         self.pw = data0['mysql']['pw']
         self.db = data0['mysql']['db']
     def get_article_list(self):
-        import pymysql
+
         conn = pymysql.connect(host= self.dbHost, user = self.dbId, password = self.pw, db = self.db, charset='utf8')
         cursor = conn.cursor()
         print("sql start")
-        sql = "select * from article"
+        # sql = "select * from article"
+        sql = "select * from `article` where `companyName` = '경향'"
         print(sql)
         # flattened_values = [item for sublist in values_to_insert for item in sublist]
         # print(flattened_values)
@@ -49,7 +32,8 @@ class ArticleParser:
             cursor.execute(sql)
             records = cursor.fetchall()
             for row in records:
-                self.url_list.append(row[3])
+                a = (row[12],row[3])
+                self.url_list.append(a)
             conn.commit()
             # print(cursor.lastrowid)
         except Exception as except_detail:
@@ -60,7 +44,7 @@ class ArticleParser:
             return True
     def parse(self):
         for i in self.url_list:
-            url = i
+            url = i[1]
             # url = self.url_list
             session = requests.Session()
             session.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
@@ -68,17 +52,39 @@ class ArticleParser:
             response = session.get(url)
             html = response.content
             # # paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-            #
             article = newspaper.Article('')
             article.set_html(html)
             article.parse()
-            print("******************")
-            print(article.title)
-            print(article.authors)
-            print(article.text)
+            a = (article.text,i[0])
+            self.article_list.append(a)
+            print("parse Done")
+    def saveToMysql(self):
+        # 기사 작성 시간은 string format으로 함 -> 나중에 다듬자
+        conn = pymysql.connect(host= self.dbHost, user = self.dbId, password = self.pw, db = self.db, charset='utf8')
+        curs = conn.cursor()
+        try:
+            with conn.cursor() as cursor:
+                print("sql start")
+                sql = "UPDATE article SET article_body = %s WHERE titleId = %s"
+                print(sql)
+                # flattened_values = [item for sublist in values_to_insert for item in sublist]
+                # print(flattened_values)
+                cursor.executemany(sql, self.article_list)
+                # print(cursor)
+            conn.commit()
+            print(cursor.lastrowid)
+        except Exception as except_detail:
+            print("pymysql.err.ProgrammingError: «{}»".format(except_detail))
+        finally:
+            #끝났으니 비우자. 그런데 곧 Mysql Update 잘 되었는지의 로직은 추가해야 할듯
+            self.article_list = []
+            conn.close()
+            print("good")
+            return True
+
 
 if __name__ == "__main__":
     articleParser = ArticleParser()
     articleParser.get_article_list()
-    print(articleParser.url_list)
     articleParser.parse()
+    articleParser.saveToMysql()
